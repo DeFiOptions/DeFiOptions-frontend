d6caea443f937dabddbc8dc2e9b3be6086372c4a
gildor <73232606+remote-gildor@users.noreply.github.com>
2021-04-10 15:17:21 +0200

    new LP contract ABI (new Buy event parameters)

--- src/pages/Invest.vue
+++ src/pages/Invest.vue
@@ -15,7 +15,7 @@
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                           <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                              Pool balance (yours)
+                              Your pool balance
                           </div>
                         
                           <div class="h5 mb-0 font-weight-bold text-gray-800">
@@ -37,7 +37,7 @@
                   <div class="row no-gutters align-items-center">
                       <div class="col mr-2">
                           <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                              Pool balance (total)
+                              Total pool balance
                           </div>
                         
                           <div class="h5 mb-0 font-weight-bold text-gray-800">
