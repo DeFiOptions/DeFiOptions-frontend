b5d6cb336eba92cb750c1bd82b6d78d2f0acb4c7
cinquemb <1519017+cinquemb@users.noreply.github.com>
2022-02-12 19:03:49 -0500

    use main prod config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/DeFiOptions-frontend/'
-    : '/'
-}
-
-/*
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/'
-}*/
+}
